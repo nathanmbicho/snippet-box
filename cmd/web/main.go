9f2709fd8a122c39903eb7306917b91db107147d
nathanmbicho <nathanmbicho@gmail.com>
2022-02-26 13:46:50 +0300

    - download "github.com/go-sql-driver/mysql" mysql driver - setup mysql connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 //hold the application-wide dependencies
@@ -13,15 +16,40 @@ type application struct {
 	infoLog  *log.Logger
 }
 
+//wrap sql.Open() and returns a sql.DB connection pool for given DSN
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	//if error id found
+	if err != nil {
+		return nil, err
+	}
+	//verify/check if connection is still alive and check errors
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
 //main function
 func main() {
 	//command-line flag
 	addr := flag.String("addr", ":4040", "HTTP network address")
+	//command-line flag for the MySQL DSN string
+	dsn := flag.String("dsn", "golang:#G0ph3r1?@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	//info and error logger
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	//pass to openDB dsn from command-line flag
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	// defer a call to db.Close(), so that the connection pool is closed before the main() function exits.
+	defer db.Close()
 
 	//initialize a new instance of application containing the dependencies
 	app := &application{
@@ -38,6 +66,6 @@ func main() {
 
 	//run server
 	infoLog.Printf("Server starting on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
