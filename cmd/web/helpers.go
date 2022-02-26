a72a61df8ec3f83ddfbfb89c1cbd92b5e96e199c
nathanmbicho <nathanmbicho@gmail.com>
2022-02-26 12:12:06 +0300

    - add helper method to centralize and handle web errors

---
+++ cmd/web/helpers.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"runtime/debug"
+)
+
+/**
+serverError to handle error 500
+*/
+func (app *application) serverError(w http.ResponseWriter, err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+	app.errorLog.Println(trace)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+/**
+clientError to send specific status code error description
+e.g -error 400 - bad request
+*/
+func (app *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+/**
+notFound to handle error 404
+*/
+func (app *application) notFound(w http.ResponseWriter) {
+	app.clientError(w, http.StatusNotFound)
+}
