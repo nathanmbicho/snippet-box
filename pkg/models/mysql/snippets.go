54ed169934fdf0bab1f740b61be53e802956bc5f
nathanmbicho <nathanmbicho@gmail.com>
2022-02-26 15:18:19 +0300

    - add insert statement and execute method and return inserted id

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -12,7 +12,23 @@ type SnippetModel struct {
 
 // Insert new snippet into db
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
+	// stmt sql execute statement
+	stmt := "INSERT INTO snippets (title, content, created, expires) VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))"
+
+	//execute statement and return sql.Result
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	//get lastInsertedId
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	//return id - convert it from int64 to int
+	return int(id), nil
 }
 
 // Get specific snipped by request id
