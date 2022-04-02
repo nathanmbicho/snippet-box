49ffdc1ac6c24ae4803fd6d31e887d299d6e30e7
nathanmbicho <nathanmbicho@gmail.com>
2022-04-03 01:57:39 +0300

    - check if time has zero value and return empty string - further check humanDate test using table-driven tests

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -21,7 +21,12 @@ type templateData struct {
 
 //humanDate function to return readable date
 func humanDate(t time.Time) string {
-	return t.Format("02 Jan 2006 at 15:04")
+	//return empty string if time has the zero value
+	if t.IsZero() {
+		return ""
+	}
+	//convert time to utc before formatting it
+	return t.UTC().Format("02 Jan 2006 at 15:04")
 }
 
 //initialize a template.FuncMap object and store it in a global variable
--- cmd/web/templates_test.go
+++ cmd/web/templates_test.go
@@ -6,12 +6,39 @@ import (
 )
 
 func TestHumanDate(t *testing.T) {
-	//initialize new time.Time object and pass it in humanDate function
-	tm := time.Date(2022, time.April, 12, 12, 0, 0, 0, time.UTC)
-	hd := humanDate(tm)
 
-	//check output from humanDate function is in format expected else use t.Errorf() function to log value and test failed
-	if hd != "12 Apr 2022 at 12:00" {
-		t.Errorf("want %q; got %q", "12 Apr 2022 at 12:00", hd)
+	//slice of anonymous structs containing the test case name, input to humanDate and result
+	tests := []struct {
+		name string
+		tm   time.Time
+		want string
+	}{
+		{
+			name: "UTC",
+			tm:   time.Date(2022, time.April, 12, 12, 0, 0, 0, time.UTC),
+			want: "12 Apr 2022 at 12:00",
+		},
+		{
+			name: "Empty",
+			tm:   time.Time{},
+			want: "",
+		},
+		{
+			name: "EAT",
+			tm:   time.Date(2022, time.April, 12, 12, 0, 0, 0, time.FixedZone("EAT", 3*60*60)),
+			want: "12 Apr 2022 at 09:00",
+		},
+	}
+
+	//loop over the test cases
+	for _, tt := range tests {
+		//use t.Run to run the subtest of each test case
+		t.Run(tt.name, func(t *testing.T) {
+			//check output from humanDate function is in format expected else use t.Errorf() function to log value and test failed
+			hd := humanDate(tt.tm)
+			if hd != tt.want {
+				t.Errorf("want %q; got %q", tt.want, hd)
+			}
+		})
 	}
 }
